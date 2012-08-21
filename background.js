560ef150d696a23b666875a37211df87d8a1ff8f
Alex Davey <alexdavey0@gmail.com>
2012-08-21 20:36:57 +0100

    Reorganised...

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.insertCSS(null, { file : 'ui.css' });
-	chrome.tabs.executeScript(null, { file : 'underscore.js' });
-	chrome.tabs.executeScript(null, { file : 'dom.js' });
-	chrome.tabs.executeScript(null, { file : 'grab.js' });
-	chrome.tabs.executeScript(null, { file : 'highlighter.js' });
-	chrome.tabs.executeScript(null, { file : 'selection.js' });
-	chrome.tabs.executeScript(null, { file : 'main.js' });
+	chrome.tabs.insertCSS(null, { file : 'src/ui.css' });
+	chrome.tabs.executeScript(null, { file : 'lib/underscore.js' });
+	chrome.tabs.executeScript(null, { file : 'src/dom.js' });
+	chrome.tabs.executeScript(null, { file : 'src/grab.js' });
+	chrome.tabs.executeScript(null, { file : 'src/highlighter.js' });
+	chrome.tabs.executeScript(null, { file : 'src/selection.js' });
+	chrome.tabs.executeScript(null, { file : 'src/main.js' });
 });
---
+++ src/main.js
@@ -0,0 +1,69 @@
+(function(window, document, grab, $, Selection, undefined) {
+
+	"use strict";
+
+	function toggleTracking() {
+		if (tracking) {
+			add.innerText = 'Add';
+			Current.hide();
+		} else {
+			add.innerText = 'Stop';
+			Current.show();
+		}
+		tracking = !tracking;
+	}
+
+	function toggleRemoval() {
+		
+	}
+
+	function startAnalysis() {
+		var model = grab.toModel(grab.same(Confirmed.elements)),
+			elements = grab.find(model),
+			extrapolated = _.difference(elements, Confirmed.elements);
+
+		Extrapolated.clear();
+		_.each(extrapolated, Extrapolated.add, Extrapolated);
+	}
+
+	function isClickable(target) {
+		return target !== controls && target !== add && 
+			target !== remove && target !== analyse &&
+			target !== document.body;
+	}
+
+	function onMouseDown(e) {
+		if (!tracking || !isClickable(e.target)) return;
+		Confirmed.add(e.target);
+		e.preventDefault();
+		if (Confirmed.size() > 2) startAnalysis();
+	}
+
+	function onMouseMove(e) {
+		if (tracking && isClickable(e.target)) {
+			Current.highlightElement(e.target);
+		}
+	}
+
+	var tracking = false,
+		Extrapolated = new Selection('.grab-extrapolated'),
+		Confirmed = new Selection('.grab-confirmed');
+
+	var Current = new Highlighter('#grab-currentSelection'),
+		controls = $.createElement(document.body, 'div'),
+		addControl = _.bind($.createElement, $, controls, 'button');
+
+	var add = addControl('Add'),
+		remove = addControl('Remove'),
+		analyse = addControl('Analyse');
+	
+	controls.id = 'grab-controls';
+
+	add.addEventListener('click', toggleTracking, false);
+	remove.addEventListener('click', toggleRemoval, false);
+	analyse.addEventListener('click', startAnalysis, false);
+
+	document.addEventListener('mousedown', onMouseDown, false);
+	document.addEventListener('mousemove', onMouseMove, false);
+
+}(window, document, __grab, __$, __Selection));
